556b28a3195ba73b5954b319ad9e92d3c754d711
ben <bh241296@gmail.com>
2024-08-29 21:37:45 +0300

    Change potential integral to also contain less allocations

--- src/StressEnergyTensor.jl
+++ src/StressEnergyTensor.jl
@@ -4,18 +4,18 @@ using EnvelopeApproximation.BubbleBasics
 import EnvelopeApproximation.BubbleBasics: ⋅
 using EnvelopeApproximation.BubblesEvolution
 using Base.Iterators
-import Base./
 import EnvelopeApproximation.SurfaceIntegration: surface_sections, BubbleSection, unit_sphere_point, bubble_point
 using StaticArrays
 using HCubature
 import HCubature: hcubature
 using Tullio
+import Base: exp
 
 TensorDirection = Union{Symbol, Tuple{Symbol, Symbol}}
 
 export TensorDirection
 
-unit_sphere_point(x:: SVector{2, Float64}) = unit_sphere_point(x...)
+unit_sphere_point(x:: SVector{2, Float64}):: Point3 = unit_sphere_point(x...)
 
 function td_integrand(x:: SVector{2, Float64}, s:: Symbol):: Float64
     if s ≡ :trace
@@ -38,9 +38,8 @@ function td_integrand(x:: SVector{2, Float64}, tensor_directions:: Matrix):: Mat
 end
 
 ⋅(p1:: Point3, k:: Vec3):: Float64 = coordinates(p1) ⋅ k
-/(p:: Point3, d:: Float64):: Point3 = Point3((coordinates(p) / d)...)
 
-function _exp(p:: Point3, k:: Vec3):: SVector{2, Float64}
+function real_exp(p:: Point3, k:: Vec3):: SVector{2, Float64}
     d = p ⋅ k
     return SVector{2, Float64}(cos(d), -sin(d))
 end
@@ -60,17 +59,32 @@ function add_section_contribution!(V:: Array{Float64, 3}, x:: SVector{2, Float64
     p = bubble_point(px..., bubble)
     c = (bubble.radius ^ 3 * ((ΔV / 3.) * measure(s) / 4π))
     @inbounds for (l, td) ∈ enumerate(tensor_directions), (j, k) ∈ enumerate(ks), i ∈ 1:2
-        V[i, j, l] += _exp(p, k)[i] * td_integrand(px, td) * c
+        V[i, j, l] += real_exp(p, k)[i] * td_integrand(px, td) * c
     end
 end
 
-function surface_integrand(x:: SVector{2, Float64}, surface_sections:: Vector{BubbleSection}, ks:: Vector{Vec3}, 
+function surface_integrand(x:: SVector{2, Float64}, sections:: Vector{BubbleSection}, ks:: Vector{Vec3}, 
                            bubbles:: Bubbles, tensor_directions:: Vector{TensorDirection}, ΔV:: Float64):: Array{Float64, 3}
     V = zeros(Float64, 2, length(ks), length(tensor_directions))
-    for section in surface_sections
+    for section in sections
         add_section_contribution!(V, x, section, ks, bubbles[section.bubble_index], tensor_directions, ΔV)
     end
     return V
+end
+
+function unit_sphere_hcubature(f; kwargs...)
+    x0, xf = SVector{2, Float64}(0., -1.), SVector{2, Float64}(2π, 1.)
+    return hcubature(f, x0, xf; kwargs...)
+end
+
+function surface_integral(ks:: Vector{Vec3}, 
+                          bubbles:: Bubbles, 
+                          tensor_directions:: Vector{TensorDirection},
+                          sections:: Vector{BubbleSection},
+                          ΔV:: Float64 = 1.; kwargs...):: Matrix{ComplexF64}
+    integrand(x:: SVector{2, Float64}):: Array{Float64, 3} = surface_integrand(x, sections, ks, bubbles, tensor_directions, ΔV)
+    V = unit_sphere_hcubature(integrand; kwargs...)[1]
+    return reshape(reinterpret(ComplexF64, V), length(ks), length(tensor_directions))
 end
 
 function surface_integral(ks:: Vector{Vec3}, 
@@ -80,10 +94,7 @@ function surface_integral(ks:: Vector{Vec3},
                           μ_resolution:: Float64,
                           ΔV:: Float64 = 1.; kwargs...):: Matrix{ComplexF64}
     sections = surface_sections(ϕ_resolution, μ_resolution, bubbles)
-    integrand(x:: SVector{2, Float64}):: Array{Float64, 3} = surface_integrand(x, sections, ks, bubbles, tensor_directions, ΔV)
-    x0, xf = SVector{2, Float64}(0., -1.), SVector{2, Float64}(2π, 1.)
-    V = hcubature(integrand, x0, xf; kwargs...)[1]
-    return reshape(reinterpret(ComplexF64, V), length(ks), length(tensor_directions))
+    surface_integral(ks, bubbles, tensor_directions, sections, ΔV; kwargs...)
 end
 
 function surface_integral(ks:: Vector{Vec3}, 
@@ -98,42 +109,61 @@ end
 
 export surface_integral
 
-unit_sphere_point(p:: BubbleSection) = unit_sphere_point(p.ϕ, p.μ)
+unit_sphere_point(p:: BubbleSection):: Point3 = unit_sphere_point(p.ϕ, p.μ)
 
-function element_projection(x:: SVector{2, Float64}, ks:: Vector{Vec3}):: Vector{Float64}
-    return unit_sphere_point(x) .⋅ ks
+function element_projection(x:: SVector{2, Float64}, k:: Vec3):: Float64
+    return unit_sphere_point(x) ⋅ k
 end
 
-function potential_integrand(ks:: Vector{Vec3}, bubbles:: Bubbles,
-                             ΔV:: Float64 = 1.):: Function
-    projection = x -> element_projection(x, ks)
+exp(p:: Point3, k:: Vec3):: ComplexF64 = begin
+        d = p ⋅ k
+        return cos(d) - im * sin(d)
+    end
+
+function add_potential_section_contribution!(V:: Vector{ComplexF64}, 
+                                             x:: SVector{2, Float64},
+                                             s:: BubbleSection,
+                                             ks:: Vector{Vec3},
+                                             bubble:: Bubble)
+    px = coordinate_transformation(x, s)
+    p = bubble_point(px..., bubble)
+    c = (bubble.radius ^ 2) * measure(s) / 4π
+    @. V += exp((p, ), ks) * element_projection((px, ), ks) * c
+end
+
+function potential_integrand(x:: SVector{2, Float64}, 
+                             sections:: Vector{BubbleSection}, 
+                             ks:: Vector{Vec3}, bubbles:: Bubbles, 
+                             ΔV:: Float64 = 1.):: Vector{ComplexF64}
     #=
     This assumes that the potential is negative within the true vacuum
     and zero outside of it.
     =#
-    c = @. im * ((-ΔV) / (ks ⋅ ks))
-    function integrand(x:: SVector{2, Float64}, bubble_index:: Int):: Vector{ComplexF64}
-        p = bubble_point(x..., bubble_index, bubbles)
-        return @. _exp((p, ), ks) * $projection(x) * c
+    c:: Vector{ComplexF64} = @. im * ((-ΔV) / (ks ⋅ ks))
+    V = zeros(ComplexF64, length(ks))
+    for s in sections
+        add_potential_section_contribution!(V, x, s, ks, bubbles[s.bubble_index])
     end
-end
-
-function potential_section_average(ks:: Vector{Vec3}, bubbles:: Bubbles,
-                                   ΔV:: Float64 = 1.; kwargs...)
-    integrand = potential_integrand(ks, bubbles, ΔV)
-    function _section_average(p:: BubbleSection):: Vector{ComplexF64}
-        return (1 / measure(p)) * hcubature(x -> integrand(x, p.bubble_index), p; kwargs...)[1]
-    end 
-    return _section_average
+    return c .* V
 end
 
 function potential_integral(ks:: Vector{Vec3}, 
                             bubbles:: Bubbles, 
-                            ϕ_resolution:: Float64, 
-                            μ_resolution:: Float64, 
-                            ΔV:: Float64 = 1.; kwargs...)
-    integrand = potential_section_average(ks, bubbles, ΔV; kwargs...)
-    return surface_integral(integrand, bubbles, ϕ_resolution, μ_resolution)
+                            sections:: Vector{BubbleSection},
+                            ΔV:: Float64 = 1.; kwargs...):: Vector{ComplexF64}
+    integrand(x:: SVector{2, Float64}):: Vector{ComplexF64} = potential_integrand(x, 
+                                                                                  sections,
+                                                                                  ks, bubbles, ΔV)
+    return unit_sphere_hcubature(integrand; kwargs...)[1]
+end
+
+function potential_integral(ks:: Vector{Vec3}, 
+                            bubbles:: Bubbles, 
+                            ϕ_resolution:: Float64,
+                            μ_resolution:: Float64,
+                            ΔV:: Float64 = 1.; kwargs...):: Vector{ComplexF64}
+    sections = surface_sections(ϕ_resolution, μ_resolution, bubbles)
+    return potential_integral(ks, bubbles, sections, ΔV; kwargs...)
 end
 
 function potential_integral(ks:: Vector{Vec3}, 
@@ -159,15 +189,15 @@ function T_ij(ks:: Vector{Vec3},
               tensor_directions:: Union{Vector, Nothing} = nothing; 
               kwargs...):: Array{ComplexF64, 2}
     isnothing(tensor_directions) && (tensor_directions = vcat([:trace], upper_right))
-    T = surface_integral(ks, bubbles, tensor_directions, ϕ_resolution,
-                         μ_resolution, ΔV; kwargs...)
+    sections = surface_sections(ϕ_resolution, μ_resolution, bubbles)
+    T = surface_integral(ks, bubbles, tensor_directions, sections, 
+                         ΔV; kwargs...)
      # Based on the following:
     ```math
     T_ij = ∂_iϕ∂_jϕ - δ_ij⋅L ≈ ∂_iϕ∂_jϕ - δ_ij V
     ```         
     if any(td in tensor_directions for td in diagonal)
-        vi = potential_integral(ks, bubbles, ϕ_resolution, 
-                                μ_resolution, ΔV; kwargs...)
+        vi = potential_integral(ks, bubbles, sections, ΔV; kwargs...)
         for (i, td) in enumerate(tensor_directions)
             if td in diagonal
                 T[:, i] .-= vi
