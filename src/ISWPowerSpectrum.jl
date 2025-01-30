b6b7901e52a523bebdf0ecde1a875c2cb11fa9e3
ben <bh241296@gmail.com>
2025-01-30 10:59:39 +0200

    Fix bugs in the computation of the surface contrinution to the power spectrum

--- src/ISWPowerSpectrum.jl
+++ src/ISWPowerSpectrum.jl
@@ -6,7 +6,7 @@ using EnvelopeApproximation.BubbleBasics
 import EnvelopeApproximation.ChebyshevCFT: First3MomentsChebyshevPlan
 import EnvelopeApproximation.BubblesEvolution: BubblesSnapShot
 import EnvelopeApproximation.GeometricStressEnergyTensor: align_ẑ, Δ
-import EnvelopeApproximation.GravitationalPotentials: ψ
+import EnvelopeApproximation.GravitationalPotentials: ψ, surface_ψ
 
 *(rot:: SMatrix{3, 3, Float64}, p:: Point3):: Point3 = Point3(rot * p.coordinates)
 
@@ -57,17 +57,17 @@ function surface_integrand(ks:: AbstractVector{Float64},
     _snap = rot * snapshot
 # This ignores the difference between ψ and ϕ, because at the 
 # end of the PT, the anisotropic stress is null
-    return @. 8 * abs2(surface_ψ(ks, _snap, chebyshev_plan, _Δ; 
-                                 ΔV=ΔV, a=a, G=G, kwargs...))
+    return @. 8 * abs2($surface_ψ(ks, _snap, chebyshev_plan, _Δ; 
+                                  ΔV=ΔV, a=a, G=G, kwargs...))
 end
 
 function surface_P(ks:: AbstractVector{Float64}, snapshot:: BubblesSnapShot, 
                    chebyshev_plan:: First3MomentsChebyshevPlan{N}, 
                    _Δ:: Δ; ΔV:: Float64 = 1., a:: Float64 = 1.,
                     G:: Float64 = 1., kwargs...):: Vector{Float64} where N
-    p(ΦΘ:: SVector{2, Float64}):: Vector{ComplexF64} = _integrand(ks, ΦΘ, snapshot, 
-                                                                  chebyshev_plan, _Δ; ΔV=ΔV,
-                                                                  a=a, G=G, kwargs...)
+    p(ΦΘ:: SVector{2, Float64}):: Vector{ComplexF64} = surface_integrand(ks, ΦΘ, snapshot, 
+                                                                         chebyshev_plan, _Δ; ΔV=ΔV,
+                                                                         a=a, G=G, kwargs...)
     return hcubature(p, TopHemisphereLowerLeft, TopHemisphereUpperRight; kwargs...)[1]
 end
 
